843728fd52b12f2040ce2e71d61bb50167dab53f
RajveerCodes22 <rajveersoni1525@gmail.com>
2026-07-13 16:02:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.post(
 )
 
 app.use(cors(
-    {origin :  "http://localhost:5173",
+    {origin : ["http://localhost:5173", "https://vercel-frontend-five-ebon.vercel.app"]
     credentials : true,
     methods : ["GET" , "POST", "PUT", "DELETE", "OPTIONS"]
     }
